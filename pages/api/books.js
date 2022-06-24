fa602333e949a53c926ad4dac262ddee076a1273
Sketch <11874768+SketchNI@users.noreply.github.com>
2022-06-25 00:12:13 +0100

    Transform the data from GoodReads stupid csv-style data which was hastily converted to JSON into a json object we actually like and want to use. Not all fields are migrated because I don't need them (yet...). Also normalised a lot of the data because GoodReads had empty strings rather than null :D (That may have been the CSV->JSON conversion)

--- pages/api/books.js
+++ pages/api/books.js
@@ -6,5 +6,32 @@ export default async function handler(req, res) {
     const url = 'https://gist.githubusercontent.com/SketchNI/bb348e4d651bae88034c2cad23d68292/raw/7380401b64dd327ffb75d63c64546a3fb169ed70/goodreads_books.json';
 
     const data = await axios.get(url);
-    res.status(200).json(data.data);
+
+    const book_array = [];
+    for (let i = 0; i < data.data.length; i++) {
+        let book = data.data[i];
+        book_array.push({
+            book_id: book['book id'],
+            title: book.title,
+            author: book.author,
+            additional_authors: book.additional_authors === '' ? null : book.additional_authors,
+            publisher: book.publisher,
+            isbn: book.isbn === '' ? null : book.isbn,
+            ratings: {
+                mine: book['my rating'],
+                average: book['average_rating']
+            },
+            binding: book.binding === '' ? null : book.binding,
+            length: `${book['number of pages']} pages`,
+            dates: {
+                published: book['original publication year'],
+                read: book['date read'] === '' ? null : book['date read'],
+                added: book['date added']
+            },
+            review: book.review === '' ? null : book.review,
+            read_count: book['read count'],
+        })
+    }
+
+    res.status(200).json(book_array);
 }
