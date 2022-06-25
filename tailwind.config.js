5e26227c8e671bbbddb3f12203e8106ab32847fc
Sketch <11874768+SketchNI@users.noreply.github.com>
2022-06-25 01:57:57 +0100

    I'm going to go ✨mental✨

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./styles/**/*.module.css"
+    "./styles/**/*.css"
   ],
   theme: {
     extend: {},
